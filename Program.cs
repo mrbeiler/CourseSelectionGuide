26bb3f79bbf9b9ea1c15ce860e8cb9498d043837
Jonathan <jcbeiler14@gmail.com>
2026-05-05 15:00:38 -0400

    Supabase classes

--- Program.cs
+++ Program.cs
@@ -9,4 +9,12 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<SupabaseService>();
 
+builder.Services.AddHttpClient<SupabaseService>(client =>
+{
+    client.DefaultRequestHeaders.Add("apikey", builder.Configuration["Supabase:Key"]);
+    client.DefaultRequestHeaders.Add("Authorization", $"Bearer {builder.Configuration["Supabase:Key"]}");
+});
+
+builder.Services.AddScoped<ClassListService>();
+
 await builder.Build().RunAsync();
